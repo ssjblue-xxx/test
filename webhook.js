79a690377bdc369f5150a4ebd3386bf35716680d
ssjblue-xxx <76740788+ssjblue-xxx@users.noreply.github.com>
2023-04-06 11:27:02 +0200

    Update webhook.js

--- webhook.js
+++ webhook.js
@@ -2,14 +2,14 @@ function sendMessage() {
   var wh = document.getElementById('webhook');
   var msg = document.getElementById('message');
   const request = new XMLHttpRequest();
-  request.open("post", wh.textContent);
+  request.open("post", wh.value);
 
   request.setRequestHeader('Content-type', 'application/json');
 
   const params = {
     username: "Test",
     avatar_url: "",
-    content: msg.textContent
+    content: msg.value
   }
   var data = JSON.stringify(params);
   request.send(data);
